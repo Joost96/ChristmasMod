7680ca74f7a3520c1068a1b04f63517aa3d55e74
Will Stephenson <absorr@live.com>
2012-12-09 19:29:31 -0500

    Started on Santa AI
    did not really get anywhere yet

--- ChristmasMod Source/common/ChristmasMod/common/EntitySanta.java
+++ ChristmasMod Source/common/ChristmasMod/common/EntitySanta.java
@@ -1,13 +1,8 @@
 package ChristmasMod.common;
 
-import net.minecraft.src.EntityAISwimming;
-import net.minecraft.src.EntityAIWander;
-import net.minecraft.src.EntityAgeable;
-import net.minecraft.src.EntityAnimal;
-import net.minecraft.src.EntityMob;
-import net.minecraft.src.World;
+import net.minecraft.src.*;
 
-public class EntitySanta extends EntityMob
+public class EntitySanta extends EntityGolem implements IRangedAttackMob
 {
 
 	public EntitySanta(World par1World) {
@@ -16,6 +11,9 @@ public class EntitySanta extends EntityMob
 		this.moveSpeed = 0.25F;
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		this.tasks.addTask(0, new EntityAIWander(this, 0.25F));
+		this.tasks.addTask(1, new EntityAIArrowAttack(this, 0.25F, 20, 10.0F));
+		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityLiving.class, 16.0F, 0, true, false, IMob.mobSelector));
 	}
 	protected boolean isAIEnabled()
 	{
@@ -44,6 +42,18 @@ public class EntitySanta extends EntityMob
 	@Override
 	public int getMaxHealth() {
 		return 20;
+	}
+	@Override
+	public void attackEntityWithRangedAttack(EntityLiving par1EntityLiving) 
+	{
+		EntitySnowball var2 = new EntitySnowball(this.worldObj, this);
+        double var3 = par1EntityLiving.posX - this.posX;
+        double var5 = par1EntityLiving.posY + (double)par1EntityLiving.getEyeHeight() - 1.100000023841858D - var2.posY;
+        double var7 = par1EntityLiving.posZ - this.posZ;
+        float var9 = MathHelper.sqrt_double(var3 * var3 + var7 * var7) * 0.2F;
+        var2.setThrowableHeading(var3, var5 + (double)var9, var7, 1.6F, 12.0F);
+        this.func_85030_a("random.bow", 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
+        this.worldObj.spawnEntityInWorld(var2);
 	}
 
 }
